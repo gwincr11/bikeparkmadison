b44e1a9ebc6f9c5ea137c079cbb2bc21eb51f612
Cory Gwin <gwincr11@yahoo.com>
2012-10-20 10:43:57 -0500

    initial git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikepark::Application.config.secret_token = '3e266f5998529526e9a6ff27da2c721b7a60c4e739665abdf80d30439c96732958b26577ea1a4d58304520b6d26f2ec28f1da42f7575539807e4299446b102fa'
